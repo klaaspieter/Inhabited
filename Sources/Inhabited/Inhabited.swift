13899e91274d23a035a6566ba928972f813b01e0
Klaas Pieter Annema <kpa@annema.me>
2018-03-14 22:12:15 +0100

    Conform to Collection

--- Sources/Inhabited/Inhabited.swift
+++ Sources/Inhabited/Inhabited.swift
@@ -1,5 +1,35 @@
 struct Inhabited<C: Collection> {
+  private let collection: C
+
   init?(_ collection: C) {
     guard !collection.isEmpty else { return nil }
+    self.collection = collection
+  }
+}
+
+extension Inhabited: Collection {
+  typealias Index = C.Index
+  typealias Iterator = C.Iterator
+
+  var startIndex: C.Index {
+    return collection.startIndex
+  }
+
+  var endIndex: C.Index {
+    return collection.endIndex
+  }
+
+  subscript(position: C.Index) -> C.Element {
+    return collection[position]
+  }
+
+  func index(after i: C.Index) -> C.Index {
+    return collection.index(after: i)
+  }
+}
+
+extension Inhabited: Sequence {
+  func makeIterator() -> C.Iterator {
+    return collection.makeIterator()
   }
 }
--- Tests/InhabitedTests/InhabitedTests.swift
+++ Tests/InhabitedTests/InhabitedTests.swift
@@ -14,6 +14,34 @@ class InhabitedTests: XCTestCase {
     XCTAssertNotNil(collection)
   }
 
+  func testStartIndexArray() {
+    let array = [1]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(collection?.startIndex, array.startIndex)
+  }
+
+  func testEndIndexArray() {
+    let array = [1]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(collection?.endIndex, array.endIndex)
+  }
+
+  func testSubscriptArray() {
+    let array = [1, 2, 3]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(collection?[2], array[2])
+  }
+
+  func testIndexAfterArray() {
+    let array = [1, 2, 3]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(collection?.index(after: 0), array.index(after: 0))
+  }
+
   func testInitEmptyString() {
     let collection = Inhabited("")
 
@@ -26,10 +54,48 @@ class InhabitedTests: XCTestCase {
     XCTAssertNotNil(collection)
   }
 
+  func testStartIndexString() {
+    let string = "hello"
+    let collection = Inhabited(string)
+
+    XCTAssertEqual(collection?.startIndex, string.startIndex)
+  }
+
+  func testEndIndexString() {
+    let string = "hello"
+    let collection = Inhabited(string)
+
+    XCTAssertEqual(collection?.endIndex, string.endIndex)
+  }
+
+  func testSubscriptString() {
+    let string = "hello"
+    let collection = Inhabited(string)
+
+    XCTAssertEqual(collection?[string.startIndex], string[string.startIndex])
+  }
+
+  func testIndexAfterString() {
+    let string = "hello"
+    let collection = Inhabited(string)
+
+    XCTAssertEqual(
+      collection?.index(after: string.startIndex), string.index(after: string.startIndex)
+    )
+  }
+
   static var allTests = [
     ("testInitEmptyArray", testInitEmptyArray),
     ("testInitInhabitedArray", testInitInhabitedArray),
+    ("testStartIndexArray", testStartIndexArray),
+    ("testEndIndexArray", testEndIndexArray),
+    ("testSubscriptArray", testSubscriptArray),
+    ("testIndexAfterArray", testIndexAfterArray),
     ("testInitEmptyString", testInitEmptyString),
     ("testInitInhabitedString", testInitInhabitedString),
+    ("testStartIndexString", testStartIndexArray),
+    ("testEndIndexString", testEndIndexString),
+    ("testSubscriptString", testSubscriptString),
+    ("testIndexAfterString", testIndexAfterString),
   ]
 }
