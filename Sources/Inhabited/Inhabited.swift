f3de260c58351da9d2c39f1bcbd4626c96ca51c8
Klaas Pieter Annema <kpa@annema.me>
2018-03-21 10:53:02 +0100

    Add Collection conformance

--- Sources/Inhabited/Inhabited.swift
+++ Sources/Inhabited/Inhabited.swift
@@ -1,11 +1,57 @@
 public struct Inhabited<Element> {
-  private let head: Element
-  private let tail: [Element]
+  private var head: Element
+  private var tail: [Element]
 
   public init?<C: Collection>(_ collection: C) where C.Element == Element {
     guard let head = collection.first else { return nil }
     self.head = head
     self.tail = Array(collection.dropFirst())
+  }
+}
+
+extension Inhabited: Collection {
+  public func index(after i: Int) -> Int {
+    return i + 1
+  }
+
+  public subscript(position: Int) -> Element {
+    get {
+      if position == startIndex {
+        return head
+      } else {
+        return tail[position - 1]
+      }
+    }
+
+    set {
+      if position == startIndex {
+        head = newValue
+      } else {
+        tail[position - 1] = newValue
+      }
+    }
+  }
+
+  public var startIndex: Int {
+    return 0
+  }
+
+  public var endIndex: Int {
+    return tail.endIndex + 1
+  }
+
+  public typealias Index = Int
+
+  public func makeIterator() -> AnyIterator<Element> {
+    var returnHead = true
+    var tailIterator = tail.makeIterator()
+    return AnyIterator({
+      if returnHead {
+        defer { returnHead = false }
+        return self.head
+      }
+      return tailIterator.next()
+    })
   }
 }
 
--- Tests/InhabitedTests/InhabitedTests.swift
+++ Tests/InhabitedTests/InhabitedTests.swift
@@ -39,4 +39,37 @@ class InhabitedTests: XCTestCase {
 
     XCTAssertNotEqual(collection, Inhabited(string))
   }
+
+  func testSubscriptGet() {
+    let string = "hello"
+    let collection = Inhabited(string)!
+
+    XCTAssertEqual(collection[collection.startIndex], string[string.startIndex])
+  }
+
+  func testSubscriptGetAfterStartIndex() {
+    let array = [0, 1, 2, 3]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(collection?[2], array[2])
+  }
+
+  func testSubscriptSet() {
+    let array = [1, 1, 3]
+    var collection = Inhabited(array)
+
+    collection?[1] = 2
+
+    XCTAssertEqual(collection, Inhabited([1, 2, 3]))
+  }
+
+  func testIndexAfter() {
+    let array = [0, 1, 2, 3]
+    let collection = Inhabited(array)
+
+    XCTAssertEqual(
+      collection?.index(after: 2),
+      array.index(after: 2)
+    )
+  }
 }
