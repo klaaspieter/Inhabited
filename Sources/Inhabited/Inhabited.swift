aeca9ea3d5709420ea61ce3df856eb7a5dec7f19
Klaas Pieter Annema <kpa@annema.me>
2018-03-21 10:27:19 +0100

    Redefine Inhabited as head | [tail]
    The goal is to remove all fatal errors by ensuring that `head` will
    always have a value. We might have to abandon mutating completely
    though.

--- Sources/Inhabited/Inhabited.swift
+++ Sources/Inhabited/Inhabited.swift
@@ -1,87 +1,10 @@
-public struct Inhabited<C: Collection> {
-  private var collection: C
+public struct Inhabited<Element> {
+  private let head: Element
+  private let tail: [Element]
 
-  public init?(_ collection: C) {
-    guard !collection.isEmpty else { return nil }
-    self.collection = collection
-  }
-
-  public var count: Int {
-    return collection.count
-  }
-
-  public var isEmpty: Bool {
-    return false
-  }
-
-  public var first: C.Element {
-    return collection.first!
-  }
-}
-
-extension Inhabited: RangeReplaceableCollection where C: RangeReplaceableCollection {
-  public init() {
-    preconditionFailure("An Inhabited collection cannot be initialized without elements")
-  }
-
-  public mutating func replaceSubrange<C>(
-    _ subrange: Range<Index>,
-    with newElements: C
-  ) where C : Collection, C.Element == Element {
-    var newCollection = collection
-    newCollection.replaceSubrange(subrange, with: newElements)
-
-    guard !newCollection.isEmpty else {
-      preconditionFailure("Cannot remove the last element from a Inhabited collection")
-    }
-
-    collection = newCollection
-  }
-}
-
-extension Inhabited: Equatable where C: Equatable {
-  public static func == (lhs: Inhabited<C>, rhs: Inhabited<C>) -> Bool {
-    return lhs.collection == rhs.collection
-  }
-}
-
-extension Inhabited: MutableCollection where C: MutableCollection {
-  public typealias Element = C.Element
-  public typealias SubSequence = C.SubSequence
-
-  public subscript(position: C.Index) -> C.Element {
-    get {
-      return collection[position]
-    }
-    set {
-      collection[position] = newValue
-    }
-  }
-}
-
-extension Inhabited: Collection {
-  public typealias Index = C.Index
-  public typealias Iterator = C.Iterator
-
-  public var startIndex: C.Index {
-    return collection.startIndex
-  }
-
-  public var endIndex: C.Index {
-    return collection.endIndex
-  }
-
-  public func index(after i: C.Index) -> C.Index {
-    return collection.index(after: i)
-  }
-
-  public subscript(position: C.Index) -> C.Element {
-    return collection[position]
-  }
-}
-
-extension Inhabited: Sequence {
-  public func makeIterator() -> C.Iterator {
-    return collection.makeIterator()
+  public init?<C: Collection>(_ collection: C) where C.Element == Element {
+    guard let head = collection.first else { return nil }
+    self.head = head
+    self.tail = Array(collection.dropFirst())
   }
 }
